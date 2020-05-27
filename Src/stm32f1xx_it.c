2f48d40747bef36a5dd3d957d8d410ea07bb2d20
YoussefAmir <45663642+YoussefAmir@users.noreply.github.com>
2020-05-27 05:34:07 +0200

    Update stm32f1xx_it.c

--- Src/stm32f1xx_it.c
+++ Src/stm32f1xx_it.c
@@ -31,14 +31,10 @@ extern ADC_HandleTypeDef hadc1;
 extern TIM_HandleTypeDef htim1;
 extern char s[8];
 extern int flg;
-//extern int flg2;
-//extern char out[5];
 extern int ctt;
 uint32_t res;
 extern char out[4];
 int i;
-extern char y[6];
-
 //extern volatile char line_buffer[5 + 1]; // Holding buffer with space for terminating NUL
 //extern volatile int line_valid;
 //extern uint32_t count;
@@ -211,16 +207,8 @@ if(flg == 1)
 	{
 	ctt++;
 		HAL_ADC_Start(&hadc1);
-	//HAL_ADC_Start_IT(&hadc1);
-	//s	HAL_UART_Transmit_IT(&huart1,(uint8_t *) &ctt,sizeof(ctt));
 	}
 	
-	
-	//if( ctt < (((8000000) / 10) - 1)*100)
-	//{
-//			sprintf(out, "%d\n\r", ctt);
-//		HAL_UART_Transmit_IT(&huart1,(uint8_t*)&out,sizeof(out));
-	//}
 
   /* USER CODE END SysTick_IRQn 0 */
   HAL_IncTick();
@@ -245,7 +233,6 @@ void ADC1_2_IRQHandler(void)
 
 	res = HAL_ADC_GetValue(&hadc1);
 	sprintf(out, "%d", res);
-	//flg2 = 1;
 	HAL_UART_Transmit_IT(&huart1,(uint8_t*)&out,sizeof(out));
   /* USER CODE END ADC1_2_IRQn 0 */
   HAL_ADC_IRQHandler(&hadc1);
@@ -254,71 +241,6 @@ void ADC1_2_IRQHandler(void)
   /* USER CODE END ADC1_2_IRQn 1 */
 }
 
-/**
-  * @brief This function handles TIM1 break interrupt.
-  */
-//void TIM1_BRK_IRQHandler(void)
-//{
-//  /* USER CODE BEGIN TIM1_BRK_IRQn 0 */
-//	char m = 'B';
-//	HAL_UART_Transmit_IT(&huart1,(uint8_t*)&m,sizeof(m));
-
-//  /* USER CODE END TIM1_BRK_IRQn 0 */
-//  HAL_TIM_IRQHandler(&htim1);
-//  /* USER CODE BEGIN TIM1_BRK_IRQn 1 */
-
-//  /* USER CODE END TIM1_BRK_IRQn 1 */
-//}
-
-///**
-//  * @brief This function handles TIM1 update interrupt.
-//  */
-//void TIM1_UP_IRQHandler(void)
-//{
-//  /* USER CODE BEGIN TIM1_UP_IRQn 0 */
-//char m = 'U';
-//	HAL_UART_Transmit_IT(&huart1,(uint8_t*)&m,sizeof(m));
-
-//  /* USER CODE END TIM1_UP_IRQn 0 */
-//  HAL_TIM_IRQHandler(&htim1);
-//  /* USER CODE BEGIN TIM1_UP_IRQn 1 */
-
-//  /* USER CODE END TIM1_UP_IRQn 1 */
-//}
-
-///**
-//  * @brief This function handles TIM1 trigger and commutation interrupts.
-//  */
-//void TIM1_TRG_COM_IRQHandler(void)
-//{
-//  /* USER CODE BEGIN TIM1_TRG_COM_IRQn 0 */
-////HAL_TIM_Base_Stop_IT(&htim1);
-//	//HAL_UART_Transmit_IT(&huart1,(uint8_t *) &"koko",sizeof("koko"));
-//	char m = 'M';
-//	HAL_UART_Transmit_IT(&huart1,(uint8_t*)&m,sizeof(m));
-
-//  /* USER CODE END TIM1_TRG_COM_IRQn 0 */
-//  HAL_TIM_IRQHandler(&htim1);
-//  /* USER CODE BEGIN TIM1_TRG_COM_IRQn 1 */
-
-//  /* USER CODE END TIM1_TRG_COM_IRQn 1 */
-//}
-
-///**
-//  * @brief This function handles TIM1 capture compare interrupt.
-//  */
-//void TIM1_CC_IRQHandler(void)
-//{
-//  /* USER CODE BEGIN TIM1_CC_IRQn 0 */
-//char m = 'C';
-//	HAL_UART_Transmit_IT(&huart1,(uint8_t*)&m,sizeof(m));
-
-//  /* USER CODE END TIM1_CC_IRQn 0 */
-//  HAL_TIM_IRQHandler(&htim1);
-//  /* USER CODE BEGIN TIM1_CC_IRQn 1 */
-
-//  /* USER CODE END TIM1_CC_IRQn 1 */
-//}
 
 /**
   * @brief This function handles USART1 global interrupt.
@@ -331,48 +253,7 @@ void USART1_IRQHandler(void)
   HAL_UART_IRQHandler(&huart1);
   /* USER CODE BEGIN USART1_IRQn 1 */
 	//HAL_UART_IRQHandler(&huart1);
-  	HAL_UART_Receive_IT(&huart1,(uint8_t*)&s,sizeof(s));
-
-	
-		
-	//	HAL_UART_Transmit(&huart1,(uint8_t*)&s,sizeof(s),10);
-
-
-
-  //HAL_UART_Transmit_IT(&huart1,(uint8_t *) &s,sizeof(s));
-	
-	//HAL_UART_Transmit_IT(&huart1,(uint8_t *) &s,sizeof(s));
-	//HAL_ADC_Start(&hadc1);
-	//HAL_SYSTICK_Config(7999);
-//	count++;	
-	//HAL_UART_Transmit_IT(&huart1,(uint8_t *) &out,sizeof(out));
-	
-	//__HAL_TIM_SET_AUTORELOAD(&htim1,125000);
-	//TIM1->ARR = (8*10^6) / 150;
-	//__HAL_TIM_SET_COUNTER(&htim1,(8*10^6) / 1);
-	//TIM1->ARR = (8*10^6) / 1;
-	//HAL_TIM_Base_Start_IT(&htim1);
-		
-//	if(__HAL_TIM_GET_COUNTER(&htim1) == ((8*10^6) / 80000))
-//	{
-//		HAL_TIM_Base_Stop_IT(&htim1);
-//		HAL_ADC_Start_IT(&hadc1);
-//	}
-	//__HAL_TIM_SET_PRESCALER(&htim1,125000);
-//		char k = 'l';
-//		  HAL_UART_Transmit_IT(&huart1,(uint8_t *) &k,sizeof(k));
-
-//	char str2[5];
-//	strncpy( str2, s, 5);
-//	
-//	sscanf(str2, "%d", &i);
-  
-	//((8*10^6) / 80000) - 1
-	
-	//HAL_ADC_Start(&hadc1);
-	//res = HAL_ADC_GetValue(&hadc1);
-	//sprintf(out, "%d\n\r", res);
-  /* USER CODE END USART1_IRQn 1 */
+  HAL_UART_Receive_IT(&huart1,(uint8_t*)&s,sizeof(s));	
 }
 
 /* USER CODE BEGIN 1 */
